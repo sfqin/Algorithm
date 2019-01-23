de01e075aa38954e061b7038e38fd333746c2e50
fidoqin <fidoqin@wesure.cn>
2019-01-23 19:24:23 +0800

    二次出题

---
+++ com/smartZ/day2/Main.java
@@ -0,0 +1,27 @@
+package smartZ.day2;
+
+public class Main {
+
+
+    /**
+     * 大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+     * n<=39
+     * @param n
+     * @return
+     */
+    public int Fibonacci(int n) {
+
+        return 0;
+    }
+
+
+    /**
+     * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，
+     * 所有的偶数位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+     * @param array
+     */
+    public void reOrderArray(int [] array) {
+
+    }
+
+}
