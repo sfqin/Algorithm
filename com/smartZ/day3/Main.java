a6ad54fd57cdd6ece457e64bcc06778c7bd8b261
fidoqin <fidoqin@wesure.cn>
2019-02-03 10:38:11 +0800

    day3

---
+++ com/smartZ/day3/Main.java
@@ -0,0 +1,39 @@
+package smartZ.day3;
+
+
+class Node {
+    int val;
+    Node next = null;
+
+    Node(int val){
+        this.val = val;
+    }
+}
+
+public class Main {
+
+    /**
+     * 输入一个链表，输出该链表中倒数第k个结点。
+     * @param head
+     * @param k
+     * @return
+     */
+    public Node FindKthToTail(Node head, int k) {
+
+
+        return null;
+    }
+
+
+    /**
+     * 输入一个链表，反转链表后，输出新链表的表头。
+     * @param head
+     * @return
+     */
+    public Node ReverseList(Node head) {
+
+
+        return null;
+    }
+
+}
