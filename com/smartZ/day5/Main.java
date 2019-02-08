5c4eac3610aacdc4fee886013a0d7e4b1ca5d01c
qinzhe <sfqincsu@163.com>
2019-02-08 23:27:19 +0800

    day5 字符串

---
+++ com/smartZ/day5/Main.java
@@ -0,0 +1,39 @@
+package smartZ.day5;
+
+public class Main {
+
+    /**
+     * 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,并返回它的位置, 如果没有则返回 -1（需要区分大小写）
+     * @param str
+     * @return
+     */
+    public int FirstNotRepeatingChar(String str) {
+
+        return 0;
+    }
+
+    /**
+     * 汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，就是用字符串模拟这个指令的运算结果。对于一个给定的字符序列S，
+     * 请你把其循环左移K位后的序列输出。例如，字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”。是不是很简单？OK，
+     * 搞定它！
+     * @param str
+     * @param n
+     * @return
+     */
+    public String LeftRotateString(String str,int n) {
+
+        return null;
+    }
+
+    /**
+     * 牛客最近来了一个新员工Fish，每天早晨总是会拿着一本英文杂志，写些句子在本子上。同事Cat对Fish写的内容颇感兴趣，有一天他向Fish借
+     * 来翻看，但却读不懂它的意思。例如，“student. a am I”。后来才意识到，这家伙原来把句子单词的顺序翻转了，正确的句子应该是
+     * “I am a student.”。Cat对一一的翻转这些单词顺序可不在行，你能帮助他么？
+     * @param str
+     * @return
+     */
+    public String ReverseSentence(String str) {
+
+        return null;
+    }
+}
